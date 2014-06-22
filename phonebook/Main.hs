4cd5b21cf6a5eafc2d9ece22df5613d683290ce1
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-06-22 10:07:28 -0400

    Move the problem description to Main.hs

--- phonebook/Main.hs
+++ phonebook/Main.hs
@@ -1,4 +1,33 @@
-{-# LANGUAGE DeriveGeneric #-}
+{--
+
+Build a command line tool that manages phone books, via the interface below.
+
+The goal isn't necessarily to finish (though finishing is ideal), it's
+just to make progress and be able to talk about the choices you made
+while building.
+
+
+$ phonebook create hsphonebook.pb
+or
+$ python phonebook.py create hsphonebook.pb
+created phonebook hsphonebook.pb in the current directory
+
+$ phonebook lookup Sarah hsphonebook.pb # error message on no such phonebook
+Sarah Ahmed 432 123 4321
+Sarah Apple 509 123 4567
+Sarah Orange 123 456 7890
+
+$ phonebook add 'John Michael' '123 456 4323' hsphonebook.pb # error message on duplicate name
+
+$ phonebook change 'John Michael' '234 521 2332' hsphonebook.pb # error message on not exist
+
+$ phonebook remove 'John Michael' hsphonebook.pb # error message on not exist
+
+$ phonebook reverse-lookup '312 432 5432' hsphonebook.pb
+
+You should also write tests.
+
+--}
 
 import Control.Applicative
 
